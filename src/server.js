11cd03e78f0c049ef6b3014eb36a74d754fd5cbd
TheoVillela <ville.theo@gmail.com>
2024-02-03 01:11:24 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
-app.use(admRoutes)
+// app.use(admRoutes)
 
 app.listen(3001, () => {
     console.log('Listening on port 3001');
